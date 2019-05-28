9f8d2c88aca41f9ce151407e0865ff1ef5bc88f8
Shuvoshree Shuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-05-28 23:07:22 +0600

    Update ContactController.php

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-use App\Contact;
 use Brian2694\Toastr\Facades\Toastr;
+use App\Contact;
 
 class ContactController extends Controller
 {
