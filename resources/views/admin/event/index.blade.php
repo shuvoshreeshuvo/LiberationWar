57412f280fa43fbcb5044f09af7a2d30ea4fe65e
Shuvoshree Shuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-05-28 23:12:05 +0600

    Update  event index

--- resources/views/admin/event/index.blade.php
+++ resources/views/admin/event/index.blade.php
@@ -10,11 +10,46 @@
     <div class="content">
         <div class="container-fluid">
           <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-8">
                 <a  href="{{ route('event.create') }}" class="btn btn-primary">Add New</a>
                @include('layouts.admin.include.msg')
 
-              <div class="card">
+                <div class="card-header card-header-primary">
+                  <h2 class="card-title ">পর্যায়কাল</h2>   
+                </div>
+
+                 @foreach($event as $key=>$event)
+
+                 <div class="card card-profile ">
+                    <div class="card-body">
+
+                      <a href="#pablo">
+                      <img class="card-img-top" src="{{ asset('uploads/event/'.$event->image) }}">
+                          </a>
+
+                      <h4 class="card-title">{{ $event->title }}</h4>
+                      <p class="card-description">{!! $event->content !!} </p>
+                            
+                                <a href="{{ route('event.edit',$event->id) }}" class="btn btn-info btn-sm"><i class="material-icons">mode_edit</i></a>
+                                 <form  id="delete-form-{{ $event->id }}"  action="{{ route('event.destroy',$event->id) }}" style="display:none;"method="POST">
+                                    {{ csrf_field() }}
+                                    {{ method_field('DELETE') }}
+
+                                 </form>
+                                <button type="button" class="btn btn-danger btn-sm" onclick="if(confirm('Are you sure ? confirm delect this?'))
+                                {
+                                  event.preventDefault();
+                                  document.getElementById('delete-form-{{ $event->id }}').submit();
+                                }else{
+                                  event.preventDefault();
+                                }"><i class="material-icons">delete</i></button>
+
+                               </div>
+                            </div>
+                        @endforeach                   
+                 
+
+            {{--  <div class="card">
                 <div class="card-header card-header-primary">
                   <h4 class="card-title ">All Event</h4>
                  </div>
@@ -66,7 +101,8 @@
                     </table>
                   </div>
                 </div>
-              </div>
+              </div>--}}
+              
             </div>
           </div>
         </div>
