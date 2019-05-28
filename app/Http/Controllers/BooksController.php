b6b6dbc7cf455d599fa474f42d9c5b91c1e186c7
Shuvoshree Shuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-05-28 23:07:47 +0600

    Add user book controller

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Period;
 use App\Book;
 
 class BooksController extends Controller
 {
     public function Books(){
+      $periods=Period::all();
       $book = Book::all();
-        return view('Books.Books', compact('book'));
+        return view('Books.Books', compact('periods','book'));
     }
 }
