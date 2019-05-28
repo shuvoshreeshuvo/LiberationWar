7abdcfab8e2709133fffae9e473263933b58a2a7
Shuvoshree Shuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-05-28 23:08:50 +0600

    Update footer.blade.php

--- resources/views/layouts/user/include/footer.blade.php
+++ resources/views/layouts/user/include/footer.blade.php
@@ -2,13 +2,13 @@
 
 
 <!-- Footer -->
-<footer class="page-footer font-small stylish-color-dark pt-6">
+{{--<footer class="page-footer font-small stylish-color-dark pt-6">
 
     <div class="container">
         <div class="row">
             <div class="col-md-4 ">
                 <!-- Content -->
-                <h5 class="font-weight-bold text-uppercase mt-3 mb-4">আমাদের সম্পর্কে</h5>
+                <h5 class="font-weight-bold text-uppercase mt-3 mb-4"></h5>
                 <p>বহু মানুষের আত্মত্যাগ ও বীরত্বের মাধ্যমে পাকিস্তানি হানাদার বাহিনীকে পরাজিত করে আমরা পেয়েছি আমাদের স্বাধীনতা। জাতি অর্জন করেছে বিজয়। ইতিহাসে আমরা পরিচিত হয়েছি বীর বাঙালি ও বিজয়ী জাতি হিসাবে।নতুন প্রজন্মকে মুক্তিযুদ্ধের সঠিক ইতিহাস জানতে হবে। স্বাধীনতার চেতনায় দেশকে...</p>
             </div>
 
@@ -23,6 +23,133 @@
     <!-- Copyright -->
 
   
+
+</footer>
+<!-- Footer -->--}}
+
+<!-- Footer -->
+<footer class="page-footer font-small mdb-color lighten-3 pt-4">
+
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left">
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3 mr-auto my-md-4 my-0 mt-4 mb-1">
+
+        <!-- Content -->
+        <h5 class="font-weight-bold text-uppercase mb-4">আমাদের সম্পর্কে</h5>
+        <p>বাংলাদেশের মুক্তিযুদ্ধ একদিনে আসেনি। মুক্তিযুদ্ধ বাঙালি জাতির বহু ত্যাগ ও সংগ্রামের ফসল। ৫২ ভাষা আন্দোলন থেকে শুরু করে ৭১ পর্যন্ত সংগ্রামের মধ্যে দিয়ে ১৯৭১ সালের অগ্নিঝরা মার্চে শুরু হয় মুক্তিযুদ্ধ।</p>
+        <p>বহু মানুষের আত্মত্যাগ ও বীরত্বের মাধ্যমে পাকিস্তানি হানাদার বাহিনীকে পরাজিত করে আমরা পেয়েছি আমাদের স্বাধীনতা। জাতি অর্জন করেছে বিজয়। ইতিহাসে আমরা পরিচিত হয়েছি বীর বাঙালি ও বিজয়ী জাতি হিসাবে।নতুন প্রজন্মকে মুক্তিযুদ্ধের সঠিক ইতিহাস জানতে হবে।</p>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 mx-auto my-md-4 my-0 mt-4 mb-1">
+
+        <!-- Links -->
+        <h5 class="font-weight-bold text-uppercase mb-4">About</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <p>
+              <a href="#!">PROJECTS</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              <a href="#!">ABOUT US</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              <a href="#!">BLOG</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              <a href="#!">AWARDS</a>
+            </p>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3 mx-auto my-md-4 my-0 mt-4 mb-1">
+
+        <!-- Contact details -->
+        <h5 class="font-weight-bold text-uppercase mb-4">Address</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <p>
+              <i class="fas fa-home mr-3"></i> Zindabazar, Sylhet-3100,   Bangladesh</p>
+          </li>
+          <li>
+            <p>
+              <i class="fas fa-envelope mr-3"></i> info@metrouni.edu.bd</p>
+          </li>
+          <li>
+            <p>
+              <i class="fas fa-phone mr-3"></i> +88-01610-932939</p>
+          </li>
+          <li>
+            <p>
+              <i class="fas fa-print mr-3"></i> +88-0821-713077-8</p>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 text-center mx-auto my-4">
+
+        <!-- Social buttons -->
+        <h5 class="font-weight-bold text-uppercase mb-4">Follow Us</h5>
+
+        <!-- Facebook -->
+        <a type="button" class="btn-floating btn-fb">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+        <!-- Twitter -->
+        <a type="button" class="btn-floating btn-tw">
+          <i class="fab fa-twitter"></i>
+        </a>
+        <!-- Google +-->
+        <a type="button" class="btn-floating btn-gplus">
+          <i class="fab fa-google-plus-g"></i>
+        </a>
+        <!-- Dribbble -->
+        <a type="button" class="btn-floating btn-dribbble">
+          <i class="fab fa-dribbble"></i>
+        </a>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2018 Copyright:
+    <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
 
 </footer>
 <!-- Footer -->
