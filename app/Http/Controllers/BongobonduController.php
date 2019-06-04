5448b2d60a04a309e23569c7a88cf6bc86435e89
Shuvoshree Shuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-06-04 14:01:50 +0600

    Update BongobonduController.php

--- app/Http/Controllers/BongobonduController.php
+++ app/Http/Controllers/BongobonduController.php
@@ -1,16 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use App\Period;
+use App\Ff_title;
 use App\Bongobondu;
 
 class BongobonduController extends Controller
 {
     public function Bongobondu(){
     	$periods = Period::all();
+    	$ff_titles=Ff_title::all();
     	$bongobondu = Bongobondu::all();
-        return view('bongobondu.bongobondu', compact('periods', 'bongobondu'));
+        return view('bongobondu.bongobondu', compact('periods','ff_titles', 'bongobondu'));
     }
 }
