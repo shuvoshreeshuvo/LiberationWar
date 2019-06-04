124093dc3934acd2e4882610913d9cc0ad41b49f
Shuvoshree Shuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-06-04 14:02:50 +0600

    Create Freedom_fighterController.php

---
+++ app/Http/Controllers/Freedom_fighterController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use DB;
+use App\Period;
+use App\Ff_title;
+use App\Freedom_fighter;
+
+
+
+class Freedom_fighterController extends Controller
+{
+    public function index(){
+
+      $periods=Period::all();
+      $ff_titles=Ff_title::all();
+      $freedom_fighters=Freedom_fighter::all();
+      return view('Freedom_Fighter.index',compact('periods','ff_titles','freedom_fighters'));
+    } 
+
+   
+    public function freedom_fighter($id)
+    {
+        $periods=Period::all();
+        $ff_titles=Ff_title::all();
+        $freedom_fighters = DB::table('freedom_fighters')
+                ->where('ff_title_id', $id)
+                ->get();
+            //dd($events);
+        return view('Freedom_Fighter.freedom_fighter_list',compact('periods','ff_titles','freedom_fighters'));
+    }
+
+    public function freedom_fighter_details($id)
+    {
+        $periods = Period::all();
+        $ff_titles=Ff_title::all();
+        $freedom_fighter = Freedom_fighter::find($id);
+        return view('Freedom_Fighter.freedom_fighter', compact('periods', 'ff_titles','freedom_fighter'));
+    }
+
+    
+}
+
