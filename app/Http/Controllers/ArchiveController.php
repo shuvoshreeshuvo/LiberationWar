f61f4613198c389dd9ad4f578b7f9c40ba6b10ba
Shuvoshree Shuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-06-04 14:01:41 +0600

    Update ArchiveController.php

--- app/Http/Controllers/ArchiveController.php
+++ app/Http/Controllers/ArchiveController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Period;
+use App\Ff_title;
 use App\Document;
 use App\Videofootage;
 use App\Image;
@@ -12,14 +13,16 @@ class ArchiveController extends Controller
 {
     public function Documents(){
         $periods=Period::all();
+
         $document=Document::all();
         return view('Archive.Documents',compact('document'));
     }
 
     public function Image(){
         $periods=Period::all();
+        $ff_titles=Ff_title::all();
         $image=Image::all();
-        return view('Archive.Image',compact('periods','image'));
+        return view('Archive.Image',compact('periods','ff_titles','image'));
     }
     
     public function VideoFootage(){
