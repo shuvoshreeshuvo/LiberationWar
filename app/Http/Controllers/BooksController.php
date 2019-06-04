f163913767a21dd7436a7ecb4a4e9d3e8edf86b5
Shuvoshree Shuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-06-04 14:02:04 +0600

    Update BooksController.php

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -1,16 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use App\Period;
+use App\Ff_title;
 use App\Book;
 
 class BooksController extends Controller
 {
     public function Books(){
       $periods=Period::all();
+      $ff_titles=Ff_title::all();
       $book = Book::all();
-        return view('Books.Books', compact('periods','book'));
+        return view('Books.Books', compact('periods','ff_titles','book'));
     }
 }
