dab17b8232b5aab616358d2da8630ecb7c9e56f7
Shuvoshree Shuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-06-04 14:10:11 +0600

    Update bongobondu.blade.php

--- resources/views/bongobondu/bongobondu.blade.php
+++ resources/views/bongobondu/bongobondu.blade.php
@@ -10,7 +10,7 @@
 	@foreach($bongobondu as $data)
  		<div class="img">
  			<h3 style="color: red;">{{ $data->title }}</h3>
-      		<h2 style="text-align: left;color: red;">{{ $data->subtitle }}</h2>
+      		<h4 style="text-align: left;color: red;">{{ $data->subtitle }}</h4>
       		<img style="height: 300px;width: 300px" class="rounded float-right" src="{{ asset('uploads/bongobondu/'.$data-> image) }}"/>
     	</div>
 
