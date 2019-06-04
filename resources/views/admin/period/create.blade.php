ca6be7c42455203e3b9cbeda04b940738d23d3f2
Shuvoshree Shuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-06-04 14:09:54 +0600

    Update create.blade.php

--- resources/views/admin/period/create.blade.php
+++ resources/views/admin/period/create.blade.php
@@ -45,6 +45,14 @@
                         </div>
                       </div>
                   </div>
+                   <div class="row">
+                      <div class="col-md-12">
+                        <div class="form-group label-floating">
+                          <label class="control-label">Content</label>
+                          <textarea class="ckeditor" name="content"></textarea>
+                        </div>
+                      </div>
+                  </div>
 
                   <a href="{{ route('period.index') }}" class="btn btn-danger">Back</a>
                   <button type="submit" class="btn btn-primary">Save</button>
