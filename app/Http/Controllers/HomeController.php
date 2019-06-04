0f812d91ac3bb0a8193cf35fb92001d84ca43bc8
Shuvoshree Shuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-06-04 14:03:02 +0600

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Period;
+use App\Ff_title;
 
 class HomeController extends Controller
 {
@@ -12,8 +13,9 @@ class HomeController extends Controller
 	public function index()
 	{
 		$periods = Period::all();
+		$ff_titles=Ff_title::all();
 
-		return view('home', compact('periods'));
+		return view('home', compact('periods','ff_titles'));
 	}
 
 	
