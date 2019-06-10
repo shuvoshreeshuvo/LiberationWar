da51932c38c0437f85ffdfc599e7b073f9e72c75
Shuvoshree Shuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-06-10 21:08:51 +0600

    Update image.blade.php

--- resources/views/archive/image.blade.php
+++ resources/views/archive/image.blade.php
@@ -6,8 +6,12 @@
 
 <div class="container">
   <h3 style="text-align:center; color:red;" >ছবি</h3>
-  @foreach($image as $key=>$data)
-    <a href={{ $data-> image }}><img class="images" src={{ $data-> image }} style="width:100%;max-width:300px" /> </a>
+  @foreach($archive as $data)
+
+
+   <a href={{ $data-> archive }}> <img class="images" src="{{ asset('uploads/archive/'.$data->archive ) }} " style="width:100%;max-width:300px" /> </a>
+  
+
 
 @endforeach
 
