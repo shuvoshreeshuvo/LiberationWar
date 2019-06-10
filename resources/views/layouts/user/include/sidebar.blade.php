1521b85ba6224437763bac0fbcb0d26b12181055
Shuvoshree Shuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-06-10 21:09:35 +0600

    Create sidebar.blade.php

---
+++ resources/views/layouts/user/include/sidebar.blade.php
@@ -0,0 +1,46 @@
+<style>
+
+.sidenav {
+  width: 200px;
+  position: fixed;
+  z-index: 1;
+  top: 150px;
+  left: 10px;
+  background: #eee;
+  overflow-x: hidden;
+  padding: 8px 0;
+}
+
+.sidenav a {
+  padding: 6px 8px 6px 16px;
+  text-decoration: none;
+  font-size: 20px;
+  color: red;
+  display: block;
+}
+
+.sidenav a:hover {
+  color: #064579;
+}
+
+</style>
+
+ <div class="container">
+        
+<div class="col-md-3 ">
+<div class="sidenav">
+  <ul>
+     <li> <a href="#"> <i class="material-icons">keyboard_arrow_right</i>ইতিহাস</a></li>
+ <li> <a href="{!! url('/freedom_fighter'); !!}"> <i class="material-icons">keyboard_arrow_right</i>মুক্তিযোদ্ধা‎</a></li>
+  <li><a href="{!! url('/books/books'); !!}">
+    <i class="material-icons">keyboard_arrow_right</i>বই
+</a></li>
+  <li><a href="{!! url('/archive/image'); !!}"><i class="material-icons">keyboard_arrow_right</i>ছবি</a></li>
+  <li><a href="{!! url('/archive/documents'); !!}"><i class="material-icons">keyboard_arrow_right</i>ডকুমেন্টস</a></li>
+</ul>
+</div>
+
+</div>
+</div>
+
+
