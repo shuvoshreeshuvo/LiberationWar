ea18fd10cf9402c45b53286f302cbc937d1681dc
Shuvoshree Shuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-06-11 21:42:22 +0600

    Update PeriodController.php

--- app/Http/Controllers/Admin/PeriodController.php
+++ app/Http/Controllers/Admin/PeriodController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Period;
+use Carbon\Carbon;
 class PeriodController extends Controller
 {
     /**
@@ -42,11 +43,28 @@ class PeriodController extends Controller
             'starting_year' => 'required',
             'ending_year' => 'required',
         ]);
+        $image= $request->file('image');
+        $slug= str_slug($request->title);
+        if(isset($image))
+        {
+            $currentdate =Carbon::now()->toDateString();
+            $imagename =$slug .'-'. $currentdate .'-'. uniqid() .'.'.
+            $image->getClientOriginalExtension();
+            if(!file_exists('uploads/period'))
+             {
+                 mkdir('uploads/period', 0777,true);
+             }
+             $image->move('uploads/period',$imagename);
+            }else {
+
+                $imagename ='dafault.png';
+            }
 
          $period = new Period();
          $period->name = $request->name;
          $period->starting_year = $request->starting_year;
          $period->ending_year = $request->ending_year;
+         $period->image = $imagename;
          $period->content = $request->content;
          $period->save();
          return redirect()->route('period.index')->with('successMsg','Period Succesfully Saved');
@@ -89,11 +107,29 @@ class PeriodController extends Controller
             'name' => 'required'
             
         ]);
+        $period = Period::find($id);
+         $image= $request->file('image');
+        $slug= str_slug($request->title);
+        if(isset($image))
+        {
+            $currentdate =Carbon::now()->toDateString();
+            $imagename =$slug .'-'. $currentdate .'-'. uniqid() .'.'.
+            $image->getClientOriginalExtension();
+            if(!file_exists('uploads/period'))
+             {
+                 mkdir('uploads/period', 0777,true);
+             }
+             $image->move('uploads/period',$imagename);
+            }else {
 
-         $period = Period::find($id);
+                $imagename =$period->image;
+            }
+
+         
          $period->name = $request->name;
          $period->starting_year = $request->starting_year;
          $period->ending_year = $request->ending_year;
+         $period->image = $imagename;
          $period->content = $request->content;
          $period->save();
          return redirect()->route('period.index')->with('successMsg','Period Succesfully Updated');
@@ -107,7 +143,12 @@ class PeriodController extends Controller
      */
     public function destroy($id)
     {
-          Period::find($id)->delete();
+          $period=Period::find($id);
+         if(file_exists('uploads/period/'.$period->image)){
+            
+                unlink('uploads/period/'.$period->image);
+            }
+        $period->delete();  
           return redirect()->back()->with('successMsg','Period Succesfully Deleted');
     }
 }
