ab627c63e00a0d1cc33509582a0fc392b5473060
Shuvoshree Shuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-06-11 21:45:15 +0600

    Update freedom_fighter.blade.php

--- resources/views/freedom_fighter/freedom_fighter.blade.php
+++ resources/views/freedom_fighter/freedom_fighter.blade.php
@@ -9,7 +9,7 @@
   <h1 style="text-align: center;color: red;">{{ $freedom_fighter->nickname }}</h1>
    
    <div class="img">
-      <img style="height:300px;width:300px" class="rounded float-right"  src="{{ asset('/uploads/FreedomFighter/'.$freedom_fighter->image) }}" />
+      <img style="height:300px;width:300px" class="rounded float-right"  src="{{ asset('/uploads/freedom_fighter/'.$freedom_fighter->image) }}" />
     </div>
     <div class="content ">   
     	
