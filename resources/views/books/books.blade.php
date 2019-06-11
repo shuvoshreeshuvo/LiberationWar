ffb4bc475014149ea7003bcb1cac34c565745483
Shuvoshree Shuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-06-11 21:45:10 +0600

    Update books.blade.php

--- resources/views/books/books.blade.php
+++ resources/views/books/books.blade.php
@@ -6,32 +6,28 @@
 
 
 
-<div class="container">
-    <div class="row">
-        @foreach($book as $data)
-        <div class="col-md-4">
-            <div class="books" style="margin-right: 25px;margin-bottom: 20px;">
-                <div class="book">
-                    {{--<h4 style="text-align: center;color: red;">{{$data->title}}</h4>--}}
-                    <a target="_blank" href="{{URL::asset('frontend/books/'.$data->path.'.pdf') }}">
-                    <img class="bookimages" src="{{ asset('uploads/book/'.$data-> image) }}" /></a>
-                  <div class="caption">
-                    <div class="blur"></div>
-                    <div class="caption-text" style="margin-top: 20px;">
-                       {{-- <h1>{{ $data->title }}</h1> --}}
-                        <p>{!! $data->caption !!}</p>
+    <div class="container">
+
+        <div class="row">
+            @foreach($book as $data)
+                <div class="col-md-4">
+                    <div class="books" style="margin-right: 25px;margin-bottom: 20px;">
+
+                        <div class="hovereffect">
+                            <a target="_blank" href="{{URL::asset('frontend/books/'.$data->path.'.pdf') }}">
+
+                            <img class="img-responsive p-2" src="{{ asset('uploads/book/'.$data-> image) }}" alt="" width="300px" height="400px" > 
+                            <div class="overlay">
+                                <h2>{{ $data->title }}</h2>
+                                <p class="lead text-light">{{ $data->caption }}</p>
+                            </div></a>
+                        </div>
+
                     </div>
                 </div>
-                </div>
-            </div>
+            @endforeach
         </div>
-        @endforeach
     </div>
-</div>
 
 
- @endsection
-
-
-
-
+@endsection
