6f252caac1035f1a657f8b1171638642e54f0d11
Shuvoshree Shuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-06-14 00:44:44 +0600

    Update freedom_fighter.blade.php

--- resources/views/freedom_fighter/freedom_fighter.blade.php
+++ resources/views/freedom_fighter/freedom_fighter.blade.php
@@ -1,21 +1,26 @@
 @extends('layouts.front')
 
-@section('title','Our History || Freedom_Fighter')
+@section('title','Our History || Freedom Fighter')
 
 @section('content')
 
-<div class="container">
+<div class="container" style="margin-top:100px; ">
   
-  <h1 style="text-align: center;color: red;">{{ $freedom_fighter->nickname }}</h1>
+  <h1 style="text-align: center;color: red;">{{ $freedom_fighter->name }}</h1>
    
-   <div class="img">
-      <img style="height:300px;width:300px" class="rounded float-right"  src="{{ asset('/uploads/freedom_fighter/'.$freedom_fighter->image) }}" />
-    </div>
+   	@if($freedom_fighter->image)
+   		<div class="img">
+      		<img style="height:300px;width:300px" class="rounded float-right"  src="{{ asset('/uploads/freedom_fighter/'.$freedom_fighter->image) }}" />
+    	</div>
+    	@else
+    @endif
     <div class="content ">   
-    	
-       <p>{!! $freedom_fighter->content !!}</p>
+    	@if($freedom_fighter->content)
+       		<p>{!! $freedom_fighter->content !!}</p>
+        @else
+       		<p>No data found.</p>
+        @endif
     </div>
 
 </div> 
 @endsection 
-
