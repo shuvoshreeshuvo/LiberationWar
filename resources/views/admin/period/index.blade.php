79173920eea58d3a6309c74013ec267343af699d
Shuvoshree Shuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-06-14 00:44:20 +0600

    Update index.blade.php

--- resources/views/admin/period/index.blade.php
+++ resources/views/admin/period/index.blade.php
@@ -41,7 +41,7 @@
                               <td>{{ $period->starting_year }}</td>
                               <td>{{ $period->ending_year }}</td>
                               <td><img class="img-responsive img-thumbnail" src="{{ asset('uploads/period/'.$period->image) }}" style="height:100px;width:100px" alt=""></td>
-                              <td>{{ $period->content }}</td>
+                              <td>{!! $period->content !!}</td>
                               <td>{{ $period->created_at }}</td>
                               <td>{{ $period->updated_at }}</td>
                               <td> 
