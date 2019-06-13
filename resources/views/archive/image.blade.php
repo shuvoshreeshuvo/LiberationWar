7b90c60ff01e39e18cbd05cac530ced25f62419b
Shuvoshree Shuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-06-14 00:44:33 +0600

    Update image.blade.php

--- resources/views/archive/image.blade.php
+++ resources/views/archive/image.blade.php
@@ -3,13 +3,23 @@
 @section('title', 'Our History || Images')
 
 @section('content')
+<style>
+ .images{
+    border: 10px solid #fff;
+    -webkit-box-shadow: 5px 5px 5px #111;
+    box-shadow: 5px 5px 5px #111;
+  }
+  .images:hover{
+    -webkit-filter: opacity(30%);
+  }
+  </style>
 
 <div class="container">
   <h3 style="text-align:center; color:red;" >ছবি</h3>
-  @foreach($archive as $data)
+  @foreach($image as $data)
 
 
-   <a href={{ $data-> archive }}> <img class="images" src="{{ asset('uploads/archive/'.$data->archive ) }} " style="width:100%;max-width:300px" /> </a>
+   <a  href="{{ URL::to('/uploads/archive/'.$data->media) }}"> <img class="images" src="{{ asset('uploads/archive/'.$data->media ) }} " style="width:100%;height:300px;max-width:300px" /> </a>
   
 
 
