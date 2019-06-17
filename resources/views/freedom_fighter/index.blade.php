24920334f0bf2cd077465eb53aa6ee98f6106da0
Shuvoshree Shuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-06-17 23:43:55 +0600

    Update index.blade.php

--- resources/views/freedom_fighter/index.blade.php
+++ resources/views/freedom_fighter/index.blade.php
@@ -2,35 +2,31 @@
 
 @section('title', 'Our History || Freedom Fighter')
 
-@section('content') 
-
-</style>
+@section('content')
 
 <div class="container">
     <h2 style="text-align: center; color:red;" class="m-4">বাংলাদেশের স্বাধীনতা যুদ্ধের খেতাবপ্রাপ্ত মুক্তিযোদ্ধা</h2>
-    <hr>
 
-      
+
+     @foreach($ff_titles as $ff_title)    
+     <div class="content">   
+       <p>{!! $ff_title->intro !!}</p>
+    </div>
+    @endforeach
+    
       <div class="row">
         @foreach($ff_titles as $ff_title)
-        <div class="col-md-6">
-            <div class="thumbnail">
-             <div class="polaroid">
-                    <img src="{{ asset('/uploads/ff_title/'.$ff_title->image) }}"  class="img-thumbnail" width="304" height="236">
-                    <div class="caption">
-                        <p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
-                    </div>
-                </div>
-                 <p style="text-align: center;">
-                           <span class="wpb_button align_center">
-                               <a href="{!! url('/freedom_fighter/freedomfighter_title/'.$ff_title->id); !!}" class="g-btn color_green"  >
-                                   <span>Read More</span>
-                               </a>
-                           </span>
-                </p>
+        <div class="col-md-3">
+            <a href="{!! url('/freedom_fighter/freedomfighter_title/'.$ff_title->id); !!}"> <h3 style="text-align: center; color:red;" >{!! $ff_title->nickname !!}</h3></a>
+            <div class="polaroids">
+             <div class="polar">
+                    <img src="{{ asset('/uploads/ff_title/'.$ff_title->image) }}"  width="304" height="236">
+                    <p>{!! $ff_title->content !!}</p>
+             </div>
 
             </div>
-          </div>
+        </div>
+
            @endforeach
         </div>
 
