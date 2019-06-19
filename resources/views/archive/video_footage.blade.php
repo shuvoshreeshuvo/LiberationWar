7abed0ad43202fa0bbdccca704eaf97fc9538d66
shuvoshreeshuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-06-20 00:06:55 +0600

    Update video_footage.blade.php

--- resources/views/archive/video_footage.blade.php
+++ resources/views/archive/video_footage.blade.php
@@ -8,10 +8,10 @@
 
    <div class="videos">
 
-   	@foreach($Videofootage as $value) 
+   	@foreach($Videofootage as $data)
 
    		<div class="video">
-   			<iframe width="560" height="315" src="{{$value->path}}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+   			<iframe width="560" height="315" src="{{ $data->media }} " frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       	</div>
 
    	@endforeach
