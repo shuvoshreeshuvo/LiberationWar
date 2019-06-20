43b983f768ca3b7aa40022a791bc6a3cedbc53be
Shuvoshree Shuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-06-20 22:18:53 +0600

    Update Bookcategory.php

--- app/Bookcategory.php
+++ app/Bookcategory.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bookcategory extends Model
 {
-     public function books()
+    public function book()
     {
     	return $this->hasMany('App\Book');
     }
