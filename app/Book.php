0171bc233192b97a22179b64b975255cddc6247f
Shuvoshree Shuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-06-20 22:18:39 +0600

    Update Book.php

--- app/Book.php
+++ app/Book.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-     public function bookcategory()
+    public function bookcategory()
     {
     	return $this->belongsTo('App\Bookcategory');
     }
