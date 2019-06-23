a54682db4665f7a4d8b542addedecaed5172451d
shuvoshreeshuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-06-23 23:44:56 +0600

    Update Freedom_fighterController.php

--- app/Http/Controllers/Freedom_fighterController.php
+++ app/Http/Controllers/Freedom_fighterController.php
@@ -6,6 +6,7 @@ use DB;
 use App\Period;
 use App\Ff_title;
 use App\Freedom_fighter;
+use App\Bookcategory;
 
 
 
@@ -15,8 +16,9 @@ class Freedom_fighterController extends Controller
 
       $periods=Period::all();
       $ff_titles=Ff_title::all();
+      $bookcategories=Bookcategory::all();
       $freedom_fighters=Freedom_fighter::all();
-      return view('Freedom_Fighter.index',compact('periods','ff_titles','freedom_fighters'));
+      return view('Freedom_Fighter.index',compact('periods','ff_titles','freedom_fighters','bookcategories'));
     } 
 
    
@@ -24,19 +26,21 @@ class Freedom_fighterController extends Controller
     {
         $periods=Period::all();
         $ff_titles=Ff_title::all();
+        $bookcategories=Bookcategory::all();
         $freedom_fighters = DB::table('freedom_fighters')
                 ->where('ff_title_id', $id)
                 ->get();
             //dd($events);
-        return view('Freedom_Fighter.freedom_fighter_list',compact('periods','ff_titles','freedom_fighters'));
+        return view('Freedom_Fighter.freedom_fighter_list',compact('periods','ff_titles','freedom_fighters','bookcategories'));
     }
 
     public function freedom_fighter_details($id)
     {
         $periods = Period::all();
         $ff_titles=Ff_title::all();
+        $bookcategories=Bookcategory::all();
         $freedom_fighter = Freedom_fighter::find($id);
-        return view('Freedom_Fighter.freedom_fighter', compact('periods', 'ff_titles','freedom_fighter'));
+        return view('Freedom_Fighter.freedom_fighter', compact('periods', 'ff_titles','freedom_fighter','bookcategories'));
     }
 
     
