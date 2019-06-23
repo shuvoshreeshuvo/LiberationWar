24acc3669d11d8fa20522c260004eed691197169
shuvoshreeshuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-06-23 23:45:03 +0600

    Update 2019_05_15_074022_create_books_table.php

--- database/migrations/2019_05_15_074022_create_books_table.php
+++ database/migrations/2019_05_15_074022_create_books_table.php
@@ -15,7 +15,7 @@ class CreateBooksTable extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('bookcategory_id')
+            $table->integer('bookcategory_id');
            
             $table->string('title');
             $table->string('image');
