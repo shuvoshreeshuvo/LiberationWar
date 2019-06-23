2f6cc8deba8e8859ed2ce8af95f347b70585a4c8
shuvoshreeshuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-06-23 23:44:25 +0600

    Update AboutController.php

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Period;
 use App\Ff_title;
+use App\Bookcategory;
 
 
 class AboutController extends Controller
@@ -12,7 +13,8 @@ class AboutController extends Controller
     public function About(){
     	$periods=Period::all();
     	$ff_titles=Ff_title::all();
-        return view('About',compact('periods','ff_titles'));
+        $bookcategories=Bookcategory::all();
+        return view('About',compact('periods','ff_titles','bookcategories'));
     }
 
 }
