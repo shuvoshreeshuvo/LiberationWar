cf8a2cc365527fda431c0bcc05c51e3ac6bc37fd
shuvoshreeshuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-06-23 23:44:43 +0600

    Update ArchiveController.php

--- app/Http/Controllers/ArchiveController.php
+++ app/Http/Controllers/ArchiveController.php
@@ -9,6 +9,7 @@ use App\Ff_title;
 use App\Document;
 use App\Videofootage;
 use App\Archive;
+use App\Bookcategory;
 
 
 class ArchiveController extends Controller
@@ -16,29 +17,32 @@ class ArchiveController extends Controller
     public function Documents(){
         $periods=Period::all();
         $ff_titles=Ff_title::all();
+        $bookcategories=Bookcategory::all();
         $document= DB::table('archives') 
                    ->where('type', 'document')
                    ->get();
 
-        return view('archive.documents',compact('periods','ff_titles','document'));
+        return view('archive.documents',compact('periods','ff_titles','document','bookcategories'));
     }
 
     public function Image(){
         $periods=Period::all();
         $ff_titles=Ff_title::all();
+        $bookcategories=Bookcategory::all();
         $image=DB::table('archives') 
                    ->where('type', 'image')
                    ->get();
-        return view('archive.image',compact('periods','ff_titles','image'));
+        return view('archive.image',compact('periods','ff_titles','image','bookcategories'));
     }
     
     public function VideoFootage(){
         $periods=Period::all();
         $ff_titles=Ff_title::all();
+        $bookcategories=Bookcategory::all();
 		$Videofootage=DB::table('archives')
             ->where('type', 'video')
             ->get();
-        return view('archive.video_footage',compact('periods','ff_titles','Videofootage'));
+        return view('archive.video_footage',compact('periods','ff_titles','Videofootage','bookcategories'));
     }
 
 
@@ -48,6 +52,6 @@ class ArchiveController extends Controller
         $audio=DB::table('archives') 
                    ->where('type', 'audio')
                    ->get();
-        return view('archive.audio',compact('periods','ff_titles','audio'));
+        return view('archive.audio',compact('periods','ff_titles','audio','bookcategories'));
     }
 }
