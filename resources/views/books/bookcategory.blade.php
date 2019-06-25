a8adb84024fcd1b97c65ecd5134f21112e80c9e2
shuvoshreeshuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-06-25 22:53:44 +0600

    Create bookcategory.blade.php

---
+++ resources/views/books/bookcategory.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.front')
+
+@section('title','Our History || Book Category')
+
+@section('content')
+
+
+
+    <div class="container">
+
+        <div class="row">
+            bookcategory
+        </div>
+    </div>
+
+
+@endsection
