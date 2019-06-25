8296f038827794b6adbb6c891996d62ee281f13b
Shuvoshree Shuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-06-26 00:52:27 +0600

    Update video_footage.blade.php

--- resources/views/archive/video_footage.blade.php
+++ resources/views/archive/video_footage.blade.php
@@ -6,19 +6,20 @@
 <div class="container">
 	<h3 class="header" >ভিডিও তালিকা</h3>
 	<div class="row">
-		<div class="col-md-6">
+		
 
    			@foreach($Videofootage as $data)
+        <div class="col-md-6">
    		<div class="videogallery">
 
 				<iframe width="450" height="300" src="{{ $data->media }} " frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
 			<h4 class="videoTitle">{{ $data->description }}</h4>
       	</div>
-
+      </div>
    			@endforeach
 
-       </div>
+       
    </div>
 </div>
 @endsection
