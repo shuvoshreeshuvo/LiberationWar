abc73ed20dbe4285ec2c724276cb9cb2eb6dbce3
shuvoshreeshuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-07-02 00:24:15 +0600

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,12 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title> হোম || আমার ইতিহাস</title>
     <link href="{{asset('frontend/images/bd.png')}}" rel="icon">
-
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" rel="stylesheet">
+
     <link href="https://fonts.googleapis.com/css?family=Hind+Siliguri" rel="stylesheet">
+
     <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}">
 
-   <style>
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+
+
+    <style>
 
        .headertwo{
            color: red;
@@ -249,8 +256,8 @@ div.polaroid:hover .homeimage{
 
     <!--Controls-->
     <div class="controls-top">
-        <a class="btn-floating btn-secondary" href="#carousel-with-lb" data-slide="prev"><i class="fas fa-chevron-left"></i></a>
-        <a class="btn-floating btn-secondary" href="#carousel-with-lb" data-slide="next"><i class="fas fa-chevron-right"></i></a>
+        <a  href="#carousel-with-lb" data-slide="prev"><i class="fas fa-chevron-left"></i></a>
+        <a  href="#carousel-with-lb" data-slide="next"><i class="fas fa-chevron-right"></i></a>
     </div>
     <!--/.Controls-->
 
@@ -405,50 +412,50 @@ div.polaroid:hover .homeimage{
         <div class="video-grid front-page" id="front-page-videos">
             <ul class="video-list featured">
                 <li class="video featured">
-                    <a data-fancybox href="https://www.youtube.com/embed/Zq6Crtglztk?autoplay=1&controls=0&showinfo=0&color=white&rel=0" class="featured-video">
-                        <figure style="background-image: url(https://img.youtube.com/vi/Zq6Crtglztk/hqdefault.jpg);">
+                    <a data-fancybox href="https://www.youtube.com/embed/J9vUulq4tZI" class="featured-video">
+                        <figure style="background-image: url(https://img.youtube.com/vi/J9vUulq4tZI/hqdefault.jpg);">
                             <img class="videoimage" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/50598/video-thumb-placeholder-16-9.png" />
                             <figcaption>Swap-Meet Speed for Roadkill Nights</figcaption>
                         </figure>
                     </a>
                 </li>
                 <li class="video featured">
-                    <a data-fancybox href="https://www.youtube.com/embed/qDK86kzgUPg?autoplay=1&controls=0&showinfo=0&color=white&rel=0" class="featured-video">
-                        <figure style="background-image: url(https://img.youtube.com/vi/qDK86kzgUPg/hqdefault.jpg);">
-                            <img class="videoimage"  src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/50598/video-thumb-placeholder-16-9.png" />
-                            <figcaption>Cummins Diesel in a Cadillac</figcaption>
+                    <a data-fancybox href="https://www.youtube.com/embed/J9vUulq4tZI" class="featured-video">
+                        <figure style="background-image: url(https://img.youtube.com/vi/J9vUulq4tZI/hqdefault.jpg);">
+                            <img class="videoimage" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/50598/video-thumb-placeholder-16-9.png" />
+                            <figcaption>Swap-Meet Speed for Roadkill Nights</figcaption>
                         </figure>
                     </a>
                 </li>
                 <li class="video featured">
-                    <a data-fancybox href="https://www.youtube.com/embed/uSB2_428P2w?autoplay=1&controls=0&showinfo=0&color=white&rel=0" class="featured-video">
-                        <figure style="background-image: url(https://img.youtube.com/vi/uSB2_428P2w/hqdefault.jpg);">
-                            <img class="videoimage"  src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/50598/video-thumb-placeholder-16-9.png" />
-                            <figcaption>Dirt-Track Challenger Rescue and Thrash</figcaption>
+                    <a data-fancybox href="https://www.youtube.com/embed/J9vUulq4tZI" class="featured-video">
+                        <figure style="background-image: url(https://img.youtube.com/vi/J9vUulq4tZI/hqdefault.jpg);">
+                            <img class="videoimage" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/50598/video-thumb-placeholder-16-9.png" />
+                            <figcaption>Swap-Meet Speed for Roadkill Nights</figcaption>
                         </figure>
                     </a>
                 </li>
                 <li class="video featured">
-                    <a data-fancybox href="https://www.youtube.com/embed/pOhl6oVpEes?autoplay=1&controls=0&showinfo=0&color=white&rel=0" class="featured-video">
-                        <figure style="background-image: url(https://img.youtube.com/vi/pOhl6oVpEes/hqdefault.jpg);">
-                            <img class="videoimage"  src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/50598/video-thumb-placeholder-16-9.png" />
-                            <figcaption>Junkyard 1956 Buick Hack</figcaption>
+                    <a data-fancybox href="https://www.youtube.com/embed/J9vUulq4tZI" class="featured-video">
+                        <figure style="background-image: url(https://img.youtube.com/vi/J9vUulq4tZI/hqdefault.jpg);">
+                            <img class="videoimage" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/50598/video-thumb-placeholder-16-9.png" />
+                            <figcaption>Swap-Meet Speed for Roadkill Nights</figcaption>
                         </figure>
                     </a>
                 </li>
                 <li class="video featured">
-                    <a data-fancybox href="https://www.youtube.com/embed/PVs_xZ46Cxw?autoplay=1&controls=0&showinfo=0&color=white&rel=0" class="featured-video">
-                        <figure style="background-image: url(https://img.youtube.com/vi/PVs_xZ46Cxw/hqdefault.jpg);">
-                            <img class="videoimage"  src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/50598/video-thumb-placeholder-16-9.png" />
-                            <figcaption>Wheelstanding Dump Truck! Stubby Bob’s Comeback</figcaption>
+                    <a data-fancybox href="https://www.youtube.com/embed/J9vUulq4tZI" class="featured-video">
+                        <figure style="background-image: url(https://img.youtube.com/vi/J9vUulq4tZI/hqdefault.jpg);">
+                            <img class="videoimage" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/50598/video-thumb-placeholder-16-9.png" />
+                            <figcaption>Swap-Meet Speed for Roadkill Nights</figcaption>
                         </figure>
                     </a>
                 </li>
                 <li class="video featured">
-                    <a data-fancybox href="https://www.youtube.com/embed/C4jk51rgfts?autoplay=1&controls=0&showinfo=0&color=white&rel=0" class="featured-video">
-                        <figure style="background-image: url(https://img.youtube.com/vi/C4jk51rgfts/hqdefault.jpg);">
-                            <img class="videoimage"  src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/50598/video-thumb-placeholder-16-9.png" />
-                            <figcaption>Will It Go 10s? Mazdarati Is Back!</figcaption>
+                    <a data-fancybox href="https://www.youtube.com/embed/J9vUulq4tZI" class="featured-video">
+                        <figure style="background-image: url(https://img.youtube.com/vi/J9vUulq4tZI/hqdefault.jpg);">
+                            <img class="videoimage" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/50598/video-thumb-placeholder-16-9.png" />
+                            <figcaption>Swap-Meet Speed for Roadkill Nights</figcaption>
                         </figure>
                     </a>
                 </li>
@@ -463,9 +470,9 @@ div.polaroid:hover .homeimage{
 @include('layouts.user.include.footer')
 <!-- Footer -->
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
-
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 </body>
 
