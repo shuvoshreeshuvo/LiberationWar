baf4728a634a8ecfb36ad11afe4e2e12241e29c7
shuvoshreeshuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-07-09 23:52:19 +0600

    Update edit.blade.php

--- resources/views/admin/book/edit.blade.php
+++ resources/views/admin/book/edit.blade.php
@@ -64,12 +64,18 @@
                           </div>
                   </div>
 
-                   <div class="row">
-                      <div class="col-md-12">
-                        <label class="control-label">Onlinepath</label>
-                        <input type="text" class="form-control" name="path" value="{{ $book->onlinepath }}">                    
+                      <div class="row">
+                          <div class="col-md-12">
+                              <div class="form-group label-floating">
+                                  <label class="control-label">Type</label>
+                                  <select class="form-control" name="type">
+                                      <option value="{{ $book->type }}">{{ $book->type }}</option>
+                                      <option value="pdf">Pdf</option>
+                                      <option value="link">Online Link</option>
+                                  </select>
+                              </div>
                           </div>
-                  </div>
+                      </div>
 
                   <div class="row">
                       <div class="col-md-12">
