38b5016dbb820ec2d494875c1099bb6bc9ea8ce5
Shuvoshree Shuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-07-10 22:53:08 +0600

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 use App\Period;
 use App\Ff_title;
 use App\Bookcategory;
+use App\Archive;
 class HomeController extends Controller
 {
     //
@@ -15,8 +17,14 @@ class HomeController extends Controller
 		$periods = Period::all();
 		$ff_titles=Ff_title::all();
         $bookcategories=Bookcategory::all();
+        $image=DB::table('archives') 
+                   ->where('type', 'image')
+                   ->get();
+        $Videofootage=DB::table('archives')
+            ->where('type', 'video')
+            ->get();
 
-		return view('home', compact('periods','ff_titles','bookcategories'));
+		return view('home', compact('periods','ff_titles','bookcategories','image','Videofootage'));
 	}
 
 	
