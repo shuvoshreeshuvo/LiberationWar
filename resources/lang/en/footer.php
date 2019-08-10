c7d838fcb66dcddf2b164678347e210638456db4
shuvoshreeshuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-08-10 22:00:47 +0600

    Create footer.php

---
+++ resources/lang/en/footer.php
@@ -0,0 +1,12 @@
+<?php
+
+    return[
+
+        'about us' => 'The Liberation War of Bangladesh did not come in one day.The war of liberation is the result of many sacrifices and struggles of the Bengali nation.
+                        Through the struggle from the language movement the War of Liberation began on flamy march.The Pakistani invaders were defeated by the sacrifice and heroism of many people we have got our freedom.
+                        The nation has achieved victory.Throughout history we have come to be known as the brave Bengali and the winning nation.The new generation needs to know the exact history of the War of Liberation',
+
+    ];
+
+
+?>
