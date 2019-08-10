131870158300ec4a51d939cc3e7c09152be80c56
shuvoshreeshuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-08-10 22:00:49 +0600

    Create header.php

---
+++ resources/lang/en/header.php
@@ -0,0 +1,18 @@
+<?php
+
+return[
+
+    'logo' => 'Amar Etihash',
+    'home' => 'Home',
+    'history' => 'History',
+    'bongobondhu' => 'Bongobondhu',
+    'archive' => 'Archive',
+    'freedom fighter' => 'Freedom Fighter',
+    'book' => 'Book',
+    'about' => 'About us',
+    'search' => 'Search',
+
+];
+
+
+?>
