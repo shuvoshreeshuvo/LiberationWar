e3d3b89586513060732ed4024ca39e70437b142c
shuvoshreeshuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-08-10 22:01:01 +0600

    Update sidebar.blade.php

--- resources/views/layouts/user/include/sidebar.blade.php
+++ resources/views/layouts/user/include/sidebar.blade.php
@@ -32,12 +32,14 @@
 <div class="sidenav">
   <ul>
      <li> <a href="#"> <i class="material-icons">keyboard_arrow_right</i>ইতিহাস</a></li>
- <li> <a href="{!! url('/freedom_fighter'); !!}"> <i class="material-icons">keyboard_arrow_right</i>মুক্তিযোদ্ধা‎</a></li>
+      <hr>
+      <li> <a href="{!! url('/freedom_fighter'); !!}"> <i class="material-icons">keyboard_arrow_right</i>মুক্তিযোদ্ধা‎</a></li>
+      <hr>
   <li><a href="{!! url('/books/books'); !!}">
     <i class="material-icons">keyboard_arrow_right</i>বই
-</a></li>
-  <li><a href="{!! url('/archive/image'); !!}"><i class="material-icons">keyboard_arrow_right</i>ছবি</a></li>
-  <li><a href="{!! url('/archive/documents'); !!}"><i class="material-icons">keyboard_arrow_right</i>ডকুমেন্টস</a></li>
+</a></li><hr>
+  <li><a href="{!! url('/archive/image'); !!}"><i class="material-icons">keyboard_arrow_right</i>ছবি</a></li><hr>
+  <li><a href="{!! url('/archive/documents'); !!}"><i class="material-icons">keyboard_arrow_right</i>ডকুমেন্টস</a></li><hr>
 </ul>
 </div>
 
