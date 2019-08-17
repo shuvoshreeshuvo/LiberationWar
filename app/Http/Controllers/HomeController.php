3d03ba0d5b5a64f47496ef8fbab0a76196d8f429
Shuvoshree Shuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-08-18 02:08:15 +0600

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App;
 use DB;
 use App\Period;
 use App\Ff_title;
@@ -12,8 +13,11 @@ class HomeController extends Controller
 {
     //
 
-	public function index()
+	public function index1()
 	{
+    $lang  = 'bn';
+    
+    App::setLocale($lang);
 		$periods = Period::all();
 		$ff_titles=Ff_title::all();
         $bookcategories=Bookcategory::all();
@@ -49,6 +53,45 @@ class HomeController extends Controller
 
 		 return view('home', compact('periods','ff_titles','bookcategories','image1','image2','image3','Videofootage'));
 	}
+  public function index($lang)
+  {
+    
+    App::setLocale($lang);
+    $periods = Period::all();
+    $ff_titles=Ff_title::all();
+        $bookcategories=Bookcategory::all();
+        $image1=DB::table('archives') 
+                   ->where('type', 'image')
+                   ->where('special', '1')
+                   ->orderBy('id')
+                   ->take(6)
+                   ->get();
+        $image2=DB::table('archives') 
+                   ->where('type', 'image')
+                   ->where('special', '1')
+                   ->orderBy('id')
+                   ->skip(6)
+                   ->take(6)
+                   ->get();
+        $image3=DB::table('archives') 
+                   ->where('type', 'image')
+                   ->where('special', '1')
+                   ->orderBy('id')
+                   ->skip(12)
+                   ->take(6)
+                   ->get();
+        $Videofootage=DB::table('archives')
+            ->where('type', 'video')
+            ->where('special', '1')
+            ->orderBy('id')
+            ->get();
+
+           // dd($Videofootage);
+
+          //echo json_encode($Videofootage);
+
+     return view('home', compact('periods','ff_titles','bookcategories','image1','image2','image3','Videofootage'));
+  }
 
 	
 }
