6df7e06a50d2e670e572cc341b0d23d5056d4947
Shuvoshree Shuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-09-06 23:59:43 +0600

    Create EtihashController.php

---
+++ app/Http/Controllers/lang/EtihashController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+use App\Period;
+use App\Event;
+use App\Ff_title;
+use App\Bookcategory;
+
+
+class EtihashController extends Controller
+{
+   /* public function Languagemovement(){
+    	
+        $Languagemovement=Languagemovement::all();
+        return view('etihash.languagemovement.index',compact('Languagemovement'));
+    }
+
+    public function MassUprisingg(){
+
+        $Massuprisingg=Massuprisingg::all();
+        return view('etihash.massuprisingg',compact('Massuprisingg'));
+    }
+
+    public function LiberationWar(){
+
+        return view('etihash.liberationwar.liberationwar');
+    } */
+
+    
+
+    public function period_history($id)
+    {
+        $periods=Period::all();
+        $period = Period::find($id);
+        $ff_titles=Ff_title::all();
+        $bookcategories=Bookcategory::all();
+        $events = DB::table('events')
+                ->where('period_id', $id)
+                ->get();
+            //dd($events);
+        return view('etihash.languagemovement.index',compact('periods', 'period','ff_titles','events','bookcategories'));
+    }
+    public function event_brief($id)
+    {
+        $periods = Period::all();
+        $ff_titles=Ff_title::all();
+        $bookcategories=Bookcategory::all();
+        $event = Event::find($id);
+        return view('etihash.languagemovement.bongovongo', compact('periods', 'ff_titles','event','bookcategories'));
+    }
+}
