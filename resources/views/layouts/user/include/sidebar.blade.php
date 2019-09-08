205b538fa5fdd182011723ca8383820ec853d6df
shuvoshreeshuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-09-08 12:31:55 +0600

    Update sidebar.blade.php

--- resources/views/layouts/user/include/sidebar.blade.php
+++ resources/views/layouts/user/include/sidebar.blade.php
@@ -3,15 +3,15 @@
 <div class="col-md-3 ">
 <div class="sidenav">
   <ul>
-     <li> <a href="#"> <i class="material-icons">keyboard_arrow_right</i>ইতিহাস</a></li>
+     <li> <a href="#"> <i class="material-icons">keyboard_arrow_right</i>{{ __('sidebar.History') }}</a></li>
       <hr>
-      <li> <a href="{!! url('/freedom_fighter'); !!}"> <i class="material-icons">keyboard_arrow_right</i>মুক্তিযোদ্ধা‎</a></li>
+      <li> <a href="{!! url('/freedom_fighter'); !!}"> <i class="material-icons">keyboard_arrow_right</i>{{ __('sidebar.Freedom Fighters') }}</a></li>
       <hr>
-  <li><a href="{!! url('/books/books'); !!}">
-    <i class="material-icons">keyboard_arrow_right</i>বই
+  <li><a href="{!! url('/books'); !!}">
+    <i class="material-icons">keyboard_arrow_right</i>{{ __('sidebar.Books') }}
 </a></li><hr>
-  <li><a href="{!! url('/archive/image'); !!}"><i class="material-icons">keyboard_arrow_right</i>ছবি</a></li><hr>
-  <li><a href="{!! url('/archive/documents'); !!}"><i class="material-icons">keyboard_arrow_right</i>ডকুমেন্টস</a></li><hr>
+  <li><a href="{!! url('/archive/image'); !!}"><i class="material-icons">keyboard_arrow_right</i>{{ __('sidebar.Image') }}</a></li><hr>
+  <li><a href="{!! url('/archive/documents'); !!}"><i class="material-icons">keyboard_arrow_right</i>{{ __('sidebar.Documents') }}</a></li><hr>
 </ul>
 </div>
 
