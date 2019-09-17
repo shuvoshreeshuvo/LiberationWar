239ccbc560d31b7f3c4a66e710d97d8cdb4e5bb2
shuvoshreeshuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-09-17 21:14:36 +0600

    Update EtihashController.php

--- app/Http/Controllers/EtihashController.php
+++ app/Http/Controllers/EtihashController.php
@@ -18,13 +18,15 @@ class EtihashController extends Controller
     public function period_history($id)
     {
         $periods=Period::all();
+        //$period = DB::table('periods')->where('id', $id)->first();
         $period = Period::find($id);
-        $ff_titles=Ff_title::all();
-        $bookcategories=Bookcategory::all();
+        $ff_titles = Ff_title::all();
+        $bookcategories = Bookcategory::all();
         $events = DB::table('events')
                 ->where('period_id', $id)
                 ->get();
-            //dd($events);
+            //dd($bookcategories);
+        //dd(app()->getLocale());
         return view('etihash.languagemovement.index',compact('periods', 'period','ff_titles','events','bookcategories'));
     }
     public function event_brief($id)
