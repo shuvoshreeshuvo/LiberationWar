0a3bbb5223153cad5a5d85c623a78a1feb36827a
shuvoshreeshuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-10-22 18:45:20 +0600

    Update topbar.blade.php

--- resources/views/layouts/admin/include/topbar.blade.php
+++ resources/views/layouts/admin/include/topbar.blade.php
@@ -10,7 +10,17 @@
             <span class="navbar-toggler-icon icon-bar"></span>
           </button>
           <div class="collapse navbar-collapse justify-content-end">
-            
+
+              <form class="navbar-form">
+                  <div class="input-group no-border">
+                      <input type="text" value="" class="form-control" placeholder="Search...">
+                      <button type="submit" class="btn btn-white btn-round btn-just-icon">
+                          <i class="material-icons">search</i>
+                          <div class="ripple-container"></div>
+                      </button>
+                  </div>
+              </form>
+
             <ul class="navbar-nav">
               <li class="nav-item">
                 <a class="nav-link" href="#pablo">
@@ -28,22 +38,17 @@
                     Account
                   </p>
                 </a>
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownProfile">
-                  <a class="dropdown-item" href="#">Profile</a>
-                  <a class="dropdown-item" href="#">Settings</a>
-              
-                </div>
-              </li>
-              <li>
-                 <a href="{{ route('logout')}}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                             <i class="material-icons">exit_to_app</i>
-                              <p class="d-lg-none d-md-block">Logout</p>
-                  </a>
-                  <form id="logout-form" method="POST" action="{{ route('logout')}}" style="display:
+                  <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownProfile">
+                      <a class="dropdown-item" href="#">Profile</a>
+                      <a class="dropdown-item" href="#">Settings</a>
+                      <div class="dropdown-divider"></div>
+                      <a class="dropdown-item" href="{{ route('logout')}}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Log out</a>
+                      <form id="logout-form" method="POST" action="{{ route('logout')}}" style="display:
                   none">
-                 {{ csrf_field() }}
-                    
-                  </form>
+                          {{ csrf_field() }}
+
+                      </form>
+                  </div>
               </li>
             </ul>
           </div>
