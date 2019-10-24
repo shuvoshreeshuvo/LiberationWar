faf2853e140d325291fc33ab940049396066c8c2
Shuvoshree Shuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-10-25 02:14:54 +0600

    Update EtihashController.php

--- app/Http/Controllers/EtihashController.php
+++ app/Http/Controllers/EtihashController.php
@@ -13,21 +13,22 @@ use App\Bookcategory;
 class EtihashController extends Controller
 {
    
-    
 
     public function period_history($id)
     {
         $periods=Period::all();
         //$period = DB::table('periods')->where('id', $id)->first();
         $period = Period::find($id);
+        //dd($period);
         $ff_titles = Ff_title::all();
         $bookcategories = Bookcategory::all();
         $events = DB::table('events')
                 ->where('period_id', $id)
                 ->get();
-            //dd($bookcategories);
+               // dd($periods);
+           
         //dd(app()->getLocale());
-        return view('etihash.languagemovement.index',compact('periods', 'period','ff_titles','events','bookcategories'));
+       return view('etihash.languagemovement.index',compact('periods', 'period','ff_titles','events','bookcategories'));
     }
     public function event_brief($id)
     {
@@ -35,6 +36,6 @@ class EtihashController extends Controller
         $ff_titles=Ff_title::all();
         $bookcategories=Bookcategory::all();
         $event = Event::find($id);
-        return view('etihash.languagemovement.bongovongo', compact('periods', 'ff_titles','event','bookcategories'));
+       return view('etihash.languagemovement.bongovongo', compact('periods', 'ff_titles','event','bookcategories'));
     }
 }
