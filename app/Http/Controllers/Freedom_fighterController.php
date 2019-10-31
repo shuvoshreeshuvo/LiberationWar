d495992d37c8aedda4d46f80d1121af07f702572
Shuvoshree Shuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-10-31 17:38:38 +0600

    Update Freedom_fighterController.php

--- app/Http/Controllers/Freedom_fighterController.php
+++ app/Http/Controllers/Freedom_fighterController.php
@@ -27,22 +27,11 @@ class Freedom_fighterController extends Controller
         $periods=Period::all();
         $ff_titles=Ff_title::all();
         $bookcategories=Bookcategory::all();
-        //dd($bookcategories);
-        $freedom_fighters = Freedom_fighter::get();
-        //dd($freedom_fighters);
-        $t = Freedom_fighter::select('ff_title_id')->get();
-
-        $flag=0;
-    
-        /* ($freedom_fighters as $i) {
-            dd($i);
-            $flag+=1;
-        }*/
-        $freedom_fighters_filtered = DB::table('freedom_fighters')->where('ff_title_id',$freedom_fighters->id)->get();
-
-        //dd($freedom_fighters[0]);
-        //dd($id);
-        return view('Freedom_Fighter.freedom_fighter_list',compact('periods','ff_titles','freedom_fighters_filtered','bookcategories'));
+        $freedom_fighters = DB::table('freedom_fighters')
+                ->where('ff_title_id', $id)
+                ->get();
+            //dd($events);
+        return view('Freedom_Fighter.freedom_fighter_list',compact('periods','ff_titles','freedom_fighters','bookcategories'));
     }
 
     public function freedom_fighter_details($id)
@@ -56,4 +45,3 @@ class Freedom_fighterController extends Controller
 
     
 }
-
