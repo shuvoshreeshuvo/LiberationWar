26efe8946574666bab06060933f2055896c41312
shuvoshreeshuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-11-03 21:15:18 +0600

    Update freedom_fighter_list.blade.php

--- resources/views/freedom_fighter/freedom_fighter_list.blade.php
+++ resources/views/freedom_fighter/freedom_fighter_list.blade.php
@@ -6,7 +6,7 @@
 @section('content')
 
 <div class="container">
-    <h3 style="text-align: center; color:red;" class="m-4">বাংলাদেশের স্বাধীনতা যুদ্ধের খেতাবপ্রাপ্ত মুক্তিযোদ্ধা </h3>
+    <h5  class="headerfive">বাংলাদেশের স্বাধীনতা যুদ্ধের খেতাবপ্রাপ্ত মুক্তিযোদ্ধা </h5>
     <hr>
  
        <table border="1">
