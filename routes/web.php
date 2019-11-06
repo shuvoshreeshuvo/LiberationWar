8b8390e86543cad7eae0be8fbc50235efa9a5729
Shuvoshree Shuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-11-06 13:26:50 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -90,12 +90,12 @@ Route::post('/about', 'ContactController@sendMessage')->name('contact.send');
 
 Auth::routes();
 
-
+/*
 //lang
 Route::get('/locale/{lang?}', function ($lang=null) {
     App::setlocale($lang);
     return view('welcome');
-});
+});*/
 
 
 //Admin
