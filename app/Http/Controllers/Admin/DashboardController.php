e63dc7b2dcbea7a4ad3011ead94530f5a252dee1
shuvoshreeshuvo <shuvoshreeshuvo.sylhet@gmail.com>
2019-11-08 15:33:26 +0600

    Update DashboardController.php

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,18 +4,28 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Category;
-use App\Item;
-use App\Slider;
+use App\Period;
+use App\Event;
+use App\Book;
+use App\Bookcategory;
+use App\Freedom_fighter;
+use App\Ff_title;
+use App\User;
+use App\Archive;
 use App\Contact;
+
 class DashboardController extends Controller
 {
    public function index(){
-
-   	$categoryCount= Category::count();
-   	$itemCount= Item::count();
-   	$sliderCount= Slider::count();
-   	$contactCount= Contact::count();
-   	return view('admin.dashboard',compact('categoryCount','itemCount','sliderCount','contactCount'));
+        $profileCount= User::count();
+        $periodCount= Period::count();
+   	    $eventCount= Event::count();
+        $archiveCount= Archive::count();
+   	    $bookCount= Book::count();
+        $bookcategoryCount= Bookcategory::count();
+        $ff_titleCount= Ff_title::count();
+        $freedom_fighterCount= Freedom_fighter::count();
+   	    $contactCount= Contact::count();
+   	    return view('admin.dashboard',compact('profileCount','periodCount','eventCount','archiveCount','bookCount','bookcategoryCount','ff_titleCount','freedom_fighterCount','contactCount'));
    }
 }
